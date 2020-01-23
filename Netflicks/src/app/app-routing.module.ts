f89e961df41acb2e72610d5e9571f8c91d313b0f
PRATHEEK <pratheeklondhe2014@gmail.com>
2020-01-23 22:25:21 +0530

    Modified pre existing project code

--- Netflicks/src/app/app-routing.module.ts
+++ Netflicks/src/app/app-routing.module.ts
@@ -6,14 +6,6 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
   }
 ];
 
